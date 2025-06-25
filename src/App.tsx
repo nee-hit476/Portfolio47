6f94609ce52e8898257a62fa3e61769016150476
Nishidh <nishidhsingh97@gmail.com>
2025-06-25 17:28:19 +0530

    Portfolio Anatomy

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen min-w-screen flex justify-center items-center text-2xl bg-[#212121] text-white "  >
+      <span>Hello from Portfolio</span>
+    </div>
+  )
+}
